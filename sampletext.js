e9a04954b977d302bcf3e80373f11867e510ea9f
samsuanchen <samsuanchen@gmail.com>
2015-11-12 16:06:00 +0800

    update sampletext

--- sampletext.js
+++ sampletext.js
@@ -1,1 +1,1 @@
-window.sampletext="Ctrl+I to find by component\n萌 \n明 \n替換部件 畿 \n錯字：⿿虭虫礻 \n合文：⿿邏羅寶貝𩀨從䞃致招！"
+window.sampletext="Ctrl+I to find by component\n⿿婆女學 \n萌 \n⿿明日目 \n替換部件 畿 \n錯字：⿿虭虫礻 \n合文：⿿邏羅寶貝𩀨從䞃致招！"
